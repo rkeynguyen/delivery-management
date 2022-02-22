53bd7c4b982e2a44bbaa9423b7849199bbf52352
Ricky Nguyen <32655468+rkeynguyen@users.noreply.github.com>
2022-02-21 19:32:21 -0600

    added title

--- src/main/java/ics499/group4/views/HomePage.java
+++ src/main/java/ics499/group4/views/HomePage.java
@@ -1,6 +1,7 @@
 package ics499.group4.views;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -15,14 +16,17 @@ public class HomePage extends VerticalLayout {
 		addClassNames("grid", "gap-xl", "items-start", "justify-center", "max-w-screen-md", "mx-auto", "pb-l", "px-l");
 		Button customerButton = new Button("Customer");
 		Button employeeButton = new Button("Employee");
-
+		H1 title = new H1();
+		
+		title.setText("Delivery Management System");
 		customerButton.addClickListener(e -> customerButton.getUI().ifPresent(ui -> ui.navigate(CustomerView.class)));
 		employeeButton.addClickListener(e -> employeeButton.getUI().ifPresent(ui -> ui.navigate(OrderView.class)));
 
 		HorizontalLayout hl = new HorizontalLayout();
-
+		hl.addClassName("justify-center");
+		
 		hl.add(customerButton, employeeButton);
-		add(hl);
+		add(title, hl);
 
 	}
 
