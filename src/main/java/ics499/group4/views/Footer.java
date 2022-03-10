a98effb6716e9de1e85ed5f7550651a3349e6a70
Ricky Nguyen <rkeynguyen@gmail.com>
2022-03-10 02:21:43 -0600

    footer class added

---
+++ src/main/java/ics499/group4/views/Footer.java
@@ -0,0 +1,11 @@
+package ics499.group4.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class Footer extends VerticalLayout {
+
+	public Footer() {
+		getStyle().set("background", "#2f2645");
+	}
+	
+}
