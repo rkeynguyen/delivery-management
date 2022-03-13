7da7bbd7309f834c798b2538f3ed14d089c1cd86
Ricky Nguyen <rkeynguyen@gmail.com>
2022-03-13 17:22:58 -0500

    added search functionality to employee view

--- src/main/java/ics499/group4/views/EmployeeView.java
+++ src/main/java/ics499/group4/views/EmployeeView.java
@@ -3,7 +3,12 @@ package ics499.group4.views;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -17,6 +22,8 @@ public class EmployeeView extends VerticalLayout {
 
 	private Grid<Customer> grid = new Grid<>(Customer.class);
 	private Dao customers = new Dao();
+	private TextField searchField = new TextField();
+	
 	
 	public EmployeeView() {
 		setSizeFull();
@@ -27,20 +34,39 @@ public class EmployeeView extends VerticalLayout {
 		title.getStyle().set("font-size", "1.5em").set("margin", "0").set("color", "black").set("padding-right","10px");
 		title.addClickListener(e -> title.getUI().ifPresent(ui -> ui.navigate(HomePage.class)));
 		
+	    
 		//setup and update the grid
-		configureGrid();			
-		updateList();
+		configureGrid();	
 		
-		add(title, grid);
-	}
+		//data view of grid
+		GridListDataView<Customer> dataView = grid.setItems(customers.getAllCustomers());
 
-	//get all orders and updates the grid
-	private void updateList() {
-		grid.setItems(customers.getAllCustomers());
-	}
-	
-	//todo Make an updatelist that takes in parameter to grab specific items
+		//setting up the search field
+		searchField = new TextField();
+		searchField.setWidth("25%");
+		searchField.setPlaceholder("Search");
+		searchField.setPrefixComponent(new Icon(VaadinIcon.SEARCH));
+		searchField.setValueChangeMode(ValueChangeMode.EAGER);
+		searchField.addValueChangeListener(e -> dataView.refreshAll());
 
+		//handler for searching
+		dataView.addFilter(order -> {
+		    String searchTerm = searchField.getValue().trim();
+
+		    if (searchTerm.isEmpty())
+		        return true;
+		    
+		    boolean matchesTracking = order.getTrackingNumber().startsWith(searchTerm);
+		    boolean matchesFirstName = order.getFirstName().startsWith(searchTerm);
+		    boolean matchesLastName = order.getLastName().startsWith(searchTerm);
+		    boolean matchesEmail = order.getEmail().startsWith(searchTerm);
+		    boolean matchesPhone = order.getPhone().startsWith(searchTerm);
+
+		    return matchesFirstName || matchesLastName || matchesTracking || matchesEmail || matchesPhone;
+		});
+	    		
+		add(title, searchField, grid);
+	}
 	
 	//Method to set up the columns of the grid
 	private void configureGrid() {
