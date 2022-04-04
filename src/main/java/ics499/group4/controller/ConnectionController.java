bb6d42ff96b36f4038b6e4934e6f236e594cc431
sp1707 <91708407+sp1707@users.noreply.github.com>
2022-04-03 20:41:02 -0500

    DB integration with login requests
    Successfully queried for username and passwords in MySQL database and used them to correctly facilitate logins

---
+++ src/main/java/ics499/group4/controller/ConnectionController.java
@@ -0,0 +1,18 @@
+package ics499.group4.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class ConnectionController {
+    public Connection getConnection() {
+        Connection cn;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dms", "root", "bomdagoladaAAA7");
+            return cn;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/ics499/group4/controller/EmployeeController.java
+++ src/main/java/ics499/group4/controller/EmployeeController.java
@@ -1,5 +1,49 @@
 package ics499.group4.controller;
 
-public class EmployeeController {
+import ics499.group4.model.Order;
+import io.swagger.codegen.v3.templates.HandlebarTemplateEngine;
 
+import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class EmployeeController extends ConnectionController {
+    private List<Order> orderList;
+    private static EmployeeController instance = null;
+
+    private EmployeeController() {
+
+    }
+
+    public static EmployeeController instanceOf() {
+        if (instance == null) {
+            instance = new EmployeeController();
+        }
+        return instance;
+    }
+
+    //returns int due to multiple possible reasons a login request is not valid
+    //0 - login request accepted
+    //1 - username not found in db
+    //2 - username found but password does not match
+    public boolean handleLoginRequest(String username, String password) {
+        String query = "select delivery_agent_id " +
+                       "from delivery_agent " +
+                       "where username =\"" + username + "\" " +
+                       "and password =\"" + password + "\";";
+        try {
+            Connection cn = super.getConnection();
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            rs.next();
+            int agentId = rs.getInt("delivery_agent_id");
+            return agentId != -1;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
